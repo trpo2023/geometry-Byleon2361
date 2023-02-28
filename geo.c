3a411f3a1d852b504dade3bbe7a7628733b758f5
Nikita Boyvalenko <nikitab988224@gmail.com>
2023-03-01 06:01:19 +0700

    Add square and perimeter function

--- geo.c
+++ geo.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 #define PI 3.14
 typedef struct Point 
 {
     int x;
     int y;
 } point;
-
+typedef struct Vector 
+{
+    int a;
+    int b;
+} vector;
+vector funVector(point first, point second)
+{
+    vector res = {(second.x - first.x),(second.y - first.y)};
+    return res;
+}
+double lenVector(vector vec)
+{
+    return sqrt(vec.a * vec.a + vec.b * vec.b);
+}
 float pcircle(point center, int rad)
 {
     return 2 * PI * rad;
@@ -17,23 +31,33 @@ float acircle(point center, int rad)
 
 float ptriangle(point a, point b,point c, point d)
 {
-    res = 
-    return res;
+    float first = lenVector(funVector(a,b));
+    float second = lenVector(funVector(b,c));
+    float third = lenVector(funVector(c,d));
+    return first + second + third;
 }
-float striangle(point a,point b, point c, point d)
+float striangle(point a,point b, point c, point d) // По формуле Герона
 {
-    res
-    return res;
-}
+    float p = ptriangle(a,b,c,d) / 2;
+    float a = lenVector(funVector(a,b));
+    float b = lenVector(funVector(b,c));
+    float c = lenVector(funVector(c,d));
+    return sqrt(p*(p-a)*(p-b)*(p-c));
+} 
+
+
 int main()
 {
+    
     point center = {0,0};
     float rad = 3;
-    printf("P = %f; S = %f\n", pcircle(center, rad), acircle(center,rad));
+    //printf("P = %f; S = %f\n", pcircle(center, rad), acircle(center,rad));
     
     point a = {2,3};
-    point b = {3,4};
-    point c = {5,5};
-    printf("P = %f; S=%f", ptriangle(a,b,c,a), atriandle(a,b,c,a));
+    point b = {5,5};
+    point c = {9,7};
+    //printf("P = %f; S=%f", ptriangle(a,b,c,a), atriandle(a,b,c,a)); 
+    printf("%d,%d\n",funVector(a,b).a,funVector(a,b).b);
+    printf("%f",lenVector(funVector(a,b)));
     return 0;
 }
