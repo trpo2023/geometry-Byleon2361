fb643805507eef911836d15f1e796ea4768aabf0
Nikita Boyvalenko <nikitab988224@gmail.com>
2023-03-26 14:30:07 +0700

    Upgrade makefile

---
+++ src/libgeometry/geo.h
@@ -0,0 +1,21 @@
+#ifndef GEO_H
+#define GEO_H
+typedef struct Point 
+{
+    int x;
+    int y;
+} point;
+typedef struct Vector 
+{
+    int a;
+    int b;
+} vector;
+
+vector funVector(point first, point second);
+double lenVector(vector vec);
+float pcircle(point center, double rad);
+float acircle(point center, double rad);
+float ptriangle(point a, point b,point c, point d);
+float atriangle(point a,point b, point c, point d);
+
+#endif
