d5671ad173d53b368ad9d70280208f6adf7c1467
Nikita Boyvalenko <nikitab988224@gmail.com>
2023-03-26 16:09:19 +0700

    Formatted the files

--- src/libgeometry/geo.h
+++ src/libgeometry/geo.h
@@ -1,12 +1,10 @@
 #ifndef GEO_H
 #define GEO_H
-typedef struct Point 
-{
+typedef struct Point {
     int x;
     int y;
 } point;
-typedef struct Vector 
-{
+typedef struct Vector {
     int a;
     int b;
 } vector;
@@ -15,7 +13,7 @@ vector funVector(point first, point second);
 double lenVector(vector vec);
 float pcircle(point center, double rad);
 float acircle(point center, double rad);
-float ptriangle(point a, point b,point c, point d);
-float atriangle(point a,point b, point c, point d);
+float ptriangle(point a, point b, point c, point d);
+float atriangle(point a, point b, point c, point d);
 
 #endif
